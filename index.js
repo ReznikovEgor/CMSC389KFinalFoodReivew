50648a641ffa16707aed64e175cba0f0d5639a79
Sean Chapman <seankchapman@gmail.com>
2019-05-09 13:15:15 -0400

    set up local mongodb

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var exphbs = require('express-handlebars');
-var app = express();
 var restaurant = require('./models/restaurant');
 var dotenv = require('dotenv');
 var url = process.env.MONGOLAB_URI;
 
+var app = express();
 dotenv.config();
 
 app.use(logger('dev'));
@@ -17,25 +16,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use('/public', express.static('public'));
-<<<<<<< HEAD
 
 
 // Connect to MongoDB
-console.log(process.env.MONGODB)
-mongoose.connect(process.env.MONGODB,{ useNewUrlParser: true }, function(err, db) {
-    if (err) {
-        throw err;
-    } else {
-        console.log("successfully connected to the database");
-    }
-    db.close();
-});
+console.log(process.env.MONGODB);
+mongoose.connect('mongodb://localhost/cluster0');
+// mongoose.connect(process.env.MONGODB,{ useNewUrlParser: true }, function(err, db) {
+//     if (err) {
+//         throw err;
+//     } else {
+//         console.log("successfully connected to the database");
+//     }
+//     db.close();
+// });
+//mongoose.connect(process.env.MONGODB);
+
 mongoose.connection.on('error', function() {
     console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
     process.exit(1);
 });
-=======
->>>>>>> 8648fd6b8c99403a990256d370b68b1fa8c44ccc
 
 app.get('/', function(req, res) {
     res.render('home');
