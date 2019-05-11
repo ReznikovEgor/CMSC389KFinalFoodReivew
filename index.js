72667a7c2664e3ce16fb337ba93f0ac32a59cb7f
Deep <dpatel17@umd.edu>
2019-05-10 20:17:05 -0400

    app.listen

--- index.js
+++ index.js
@@ -212,7 +212,6 @@ app.get('/about', function(req, res) {
     res.render('about');
 })
 
-app.listen(3000, function() {
-    console.log('Listening on port 3000!');
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening!');
 });
-
